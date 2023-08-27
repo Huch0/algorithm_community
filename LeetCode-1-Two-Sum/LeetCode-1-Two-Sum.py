5b0cbd88ad79f749cabb492ecad1e3c0362a1f86
SeoJun <jun@SeoJunui-MacBookPro.local>
2023-08-27 19:41:33 +0900

    MOV files -파일 구조 최신화

--- LeetCode-1-Two-Sum/LeetCode-1-Two-Sum.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
-        for i in range(len(nums)-1):
-            for j in range(i+1, len(nums)):
-                if (nums[i] + nums[j] == target):
-                    return [i, j]
