35b3654959d23c58b27134c5c4dd77c78c72b388
Joo <werberr0@gmail.com>
2023-09-02 18:12:32 +0900

    Leetcode-121-BestTimeToBuyAndSellStock

---
+++ array/Leetcode-121-BestTimeToBuyAndSellStock/Leetcode-121-BestTimeToBuyAndSellStock.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        profit = 0
+        min_price = sys.maxsize
+
+        for price in prices:
+            min_price = min(min_price, price)
+            profit = max(profit, price - min_price)
+
+        return profit
+# Kadane's Algorithm
