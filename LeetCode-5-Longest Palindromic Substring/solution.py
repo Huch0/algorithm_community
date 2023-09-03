5b4b3953c5f4104896c766ef86036a1eadf55597
hhj <mnbdj12345@gmail.com>
2023-09-03 17:13:01 +0900

    fix misses

--- LeetCode-5-Longest Palindromic Substring/solution.py
+++ LeetCode-5-Longest Palindromic Substring/solution.py
@@ -1,5 +1,5 @@
-
-def longestPalindrome(s: str) -> str:
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
         max_palindrome = ""
         if len(s) == 1:
             return s
@@ -12,19 +12,13 @@ def longestPalindrome(s: str) -> str:
                 check_odd -= 1
                 if check_odd * 2 + 1 > len(max_palindrome):
                     max_palindrome =  s[i - check_odd : i + check_odd + 1]
-                    print(max_palindrome + " odd")
 
                 check_even = 0
                 while (i - check_even >= 0) and (i + 1 + check_even < len(s)) and (s[i - check_even] == s[i + 1 + check_even]):
                     check_even += 1
 
-                check_even -= 1
                 if check_even * 2 > len(max_palindrome):
+                    check_even -= 1
                     max_palindrome =  s[i - check_even : i + check_even + 2]
-                    print(max_palindrome + " even")
 
             return max_palindrome
-
-
-s = "cbbd"
-print(longestPalindrome(s))
