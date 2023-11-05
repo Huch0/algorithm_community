8db60e7d639488a4d3ac173062fa803e432e93ce
kyleidea1 <99781357+kyleidea1@users.noreply.github.com>
2023-11-05 17:35:15 +0900

    Create 1753_shortest_path.py
    백준 1753 다익스트라 알고리즘 이용하는 문제
    연습 때 작성했던 코드 적극 활용, heapq 이용하지 않으면 시간 초과 뜸. . .

---
+++ algorithm_week8/algorithm_week9/1753_shortest_path.py
@@ -0,0 +1,80 @@
+import heapq
+"""
+INF = int(1e9)
+
+n, m = map(int, input().split())
+start = int(input())
+graph = [[] for i in range(n + 1)]
+visited = [False] * (n + 1)
+distance = [INF] * (n + 1)
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+
+def get_smallest_node():
+    min_value = INF
+    index = 0
+    for i in range(1, n + 1):
+        if distance[i] < min_value and not visited[i]:
+            min_value = distance[i]
+            index = i
+    return index
+
+def dijkstra(start):
+    distance[start] = 0
+    visited[start] = True
+    for j in graph[start]:
+        distance[j[0]] = j[1]
+    for i in range(n - 1):
+        now = get_smallest_node()
+        visited[now] = True
+        # 현재 노드와 연결된 다른 노드를 확인
+        for j in graph[now]:
+            cost = distance[now] + j[1]
+            # 현재 노드를 거쳐 다른 노드로 이동하는 거리가 더 짧은 경우
+            if cost < distance[j[0]]:
+                distance[j[0]] = cost
+
+dijkstra(start)
+
+for i in range(1, n + 1):
+    if distance[i] == INF:
+        print("INF")
+    else:
+        print(distance[i])
+"""
+
+
+INF = int(1e9)
+
+n, m = map(int, input().split())
+start = int(input())
+graph = [[] for i in range(n + 1)]
+distance = [INF] * (n + 1)
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+dijkstra(start)
+
+for i in range(1, n + 1):
+    if distance[i] == INF:
+        print("INF")
+    else:
+        print(distance[i])
