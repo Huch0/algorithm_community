38383b33fe088fe112f33880a88cd99087a6a99d
Dong Gwon Kang <122734760+TonnyKang@users.noreply.github.com>
2023-11-10 16:32:44 +0900

    Baekjun-1966-PrinterQueue
    We have two queues, the first one is a normal queue with pairs as it's elements with the index and importance and the second one is a priority queue with the importances in descending order.
    Then we check the top of the first queue and check if it's the most important one. If not, we push it back to the end of the queue.
    If it is, check if the index is the one we're looking for and find which order the one we are looking for is going to be printed.
    Timecomplexity: O(N^2)*test_cases
    Spacecomplexity: O(N)

---
+++ week2/1966.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+int main() {
+    int count = 0;
+    int test_case;
+    cin >> test_case;
+    int n, m, ipt;//문서의 개수, 궁금한 문서 위치, 중요도
+    for (int i = 0; i < test_case; ++i) {
+        count = 0;
+        cin >> n >> m;
+        queue<pair<int, int>> q;
+        priority_queue<int> pq; // 우선순위 큐
+        for (int j = 0; j < n; ++j) {
+            cin >> ipt;
+            q.push({ j, ipt });
+            pq.push(ipt);
+        }
+        while (!q.empty()) {
+            int index = q.front().first;
+            int value = q.front().second;
+            q.pop();//pop it so you can put it in the back if it's wrong
+            if (pq.top() == value) {// if the value, which is the next in queue's priority matches the next priority
+                pq.pop();
+                ++count;
+                if (index == m) {
+                    cout << count << endl;
+                    break;
+                }
+            }
+            else q.push({ index,value });//if it's now suppose to be printed yet, push it back into the very end
+        }
+    }
+    cin >> n;
+}
