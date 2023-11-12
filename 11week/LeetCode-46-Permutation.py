c331a321e6c65eac2f13cd60bfc568584708cf58
SeoJun <jun@SeoJunui-MacBookPro.local>
2023-11-12 10:22:34 +0900

    LeetCode-46-Permutation.py 1. 실수로 저장 안 하고 올려서 재업로드

--- 11week/LeetCode-46-Permutation.py
+++ 11week/LeetCode-46-Permutation.py
@@ -1,3 +1,24 @@
 class Solution:
     def permute(self, nums: list[int]) -> list[list[int]]:
+        result = []
+        prev_elements = []
         
+        def dfs(elements):
+            if len(elements) == 0:
+                result.append(prev_elements[:])
+                return
+            
+            for e in elements:
+                next_elements = elements[:]
+                next_elements.remove(e)
+                
+                prev_elements.append(e)
+                dfs(next_elements)
+                prev_elements.pop()
+
+        if not nums:
+            return []
+        
+        dfs(nums)
+
+        return result
