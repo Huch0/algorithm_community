7828f33e3ff7e84eb3fb1b523cbbdc1cdfe3d836
miggule2 <miggule2@naver.com>
2023-11-26 11:42:23 +0900

    Leetcode-238-ProductofArrayExceptSelf.py

---
+++ Week4/Leetcode-238-ProductofArrayExceptSelf.py
@@ -0,0 +1,34 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        results = []
+        left_product = 1
+
+        for i in range(len(nums)):
+            results.append(left_product)
+            left_product *= nums[i]
+        
+        right_product = 1
+        for i in range(len(nums)-1,0-1,-1):
+            results[i] *= right_product
+            right_product *= nums[i]
+        
+        return results
+    
+""" 
+Time complexity O(n) solution
+    class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        results = []
+        left_product = 1
+
+        for i in range(len(nums)):
+            results.append(left_product)
+            left_product *= nums[i]
+        
+        right_product = 1
+        for i in range(len(nums)-1,0-1,-1):
+            results[i] *= right_product
+            right_product *= nums[i]
+        
+        return results
+"""
