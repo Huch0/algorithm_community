b6ef792601c5b53c7edcc30300b8413cf7afe47d
Dong Gwon Kang <122734760+TonnyKang@users.noreply.github.com>
2023-12-03 19:52:56 +0900

    [BaekJun] 2108 Statistics
    Time Complexity:
    Input Reading: Reading 'num' integers takes O(num) time.
    Loop for Input: The first loop iterating 'num' times has a time complexity of O(num).
    Sorting: The sorting operation using sort has a time complexity of O(num * log(num)).
    Loop for Counting Frequencies: The loop iterating 8001 times has a constant time complexity of O(1) because it's iterating a fixed number of times (8001).
    Final Loop: The loop iterating 8001 times also has a constant time complexity of O(1).
    Overall: The dominant factor is the sorting operation, so the overall time complexity is O(num * log(num)).
    Space Complexity:
    Vector 'arr': It stores 'num' integers, so its space complexity is O(num).
    Array 'number[8001]': It has a constant size of 8001, so its space complexity is O(1).
    Other Variables: The space complexity of other variables is negligible compared to the arrays and vectors.
    Overall: The overall space complexity is O(num).
    In summary, the time complexity is dominated by the sorting operation and is O(num * log(num)), and the space complexity is O(num)

---
+++ week5/2108.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+using namespace std;
+vector<int> arr;
+int main() {
+    int num, tmp, range, middle = 0, most_val, mean = 0;
+    int most = -9999;
+    int number[8001] = { 0, };
+    bool not_first = false;
+    cin >> num;
+    for (int i = 0; i < num; i++)
+    {
+        cin >> tmp;
+        arr.push_back(tmp);
+        mean += tmp;
+        number[tmp + 4000]++;
+    }
+    sort(arr.begin(), arr.end());
+    for (int i = 0; i < 8001; i++)
+    {
+        if (number[i] == 0)
+            continue;
+        if (number[i] == most)
+        {
+            if (not_first)
+            {
+                most_val = i - 4000;
+                not_first = false;
+            }
+        }
+        if (number[i] > most)
+        {
+            most = number[i];
+            most_val = i - 4000;
+            not_first = true;
+        }
+    }
+    middle = arr[arr.size() / 2];
+    mean = round((float)mean / num);
+    range = arr.back() - arr.front();
+    cout << mean << '\n' << middle << '\n' << most_val << '\n' << range;
+}
