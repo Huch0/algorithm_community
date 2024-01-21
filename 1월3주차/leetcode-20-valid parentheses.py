488edda66b5d2b888ab00725490fa262727c70f6
leegw1211 <leegw1211@pusan.ac.kr>
2024-01-21 19:32:01 +0900

    leetcode-20-valid parentheses

--- 1월3주차/leetcode-20-valid parentheses.py
+++ 1월3주차/leetcode-20-valid parentheses.py
@@ -1,26 +1,20 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        for char in s:
-            if char == '(' or char == '[' or char == '{':
-                stack.append(char)
-            elif len(stack) == 0:
-                return False
-            elif char == ')':
-                if stack[-1] == '(':
-                    stack.pop()
-                else:
+        mydict = {
+            '(':')',
+            '[':']',
+            '{':'}'
+        }
+        for c in s:
+            if c in mydict:
+                stack.append(c)
+            else:
+                if len(stack) == 0:
                     return False
-            elif char == ']':
-                if stack[-1] == '[':
-                    stack.pop()
-                else:
+                if c != mydict.get(stack.pop()):
                     return False
-            elif char == '}':
-                if stack[-1] == '{':
-                    stack.pop()
-                else:
-                    return False
+                    
         if len(stack) == 0:
             return True
         else:
