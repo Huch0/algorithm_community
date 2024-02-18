5134ad5aef28b5d46c2b3a36656955da7d401108
SeoJun <jun@SeoJunui-MacBookPro.local>
2024-02-18 18:24:06 +0900

    LeetCode-108-Convert-Sorted-Arr-To-BST 1. 배열의 중앙에서 출발 2. 중앙의 왼쪽을 left로 오른쪽을 right로 붙이는 재귀 구조 구현

---
+++ 23week/LeetCode-108-Convert-Sorted-Array-To-BST.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: list[int]) -> TreeNode:
+        
+        if not nums:
+            return None
+        
+        mid = len(nums) // 2
+        
+        root = TreeNode(nums[mid])
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid+1:])
+        
+        
+        return root
