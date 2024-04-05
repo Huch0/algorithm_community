cadabf84c1ce9a53ce0f528b44a0fdb23415c101
hhj <mnbdj12345@gmail.com>
2024-04-05 15:01:42 +0900

    LeetCode-134-GasStation

---
+++ week26/LeetCode-134-GasStation/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+        
+        start, total, tank = 0, 0, 0
+        
+        for i in range(len(gas)):
+            tank += gas[i] - cost[i]
+            if tank < 0:
+                start = i + 1
+                total += tank
+                tank = 0
+        
+        return start if total + tank >= 0 else -1
