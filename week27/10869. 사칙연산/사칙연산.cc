84432d877810ff9da832fd02ee5f3437919d0f0a
hhj <mnbdj12345@gmail.com>
2024-05-05 19:47:30 +0900

    baekjoon_10869

---
+++ week27/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int A, B;
+    cin >> A >> B;
+    cout << A + B << "\n";
+    cout << A - B << "\n";
+    cout << A * B << "\n";
+    cout << A / B << "\n";
+    cout << A % B << "\n";
+    
+    return 0;
+}
