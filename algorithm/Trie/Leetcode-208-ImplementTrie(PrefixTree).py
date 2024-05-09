2342aeee78fd9aeb279527a8a7e4181a9ecd737d
miggule2 <miggule2@naver.com>
2024-05-09 16:06:22 +0900

    Leetcode-208-Implement Trie (Prefix Tree)

---
+++ algorithm/Trie/Leetcode-208-ImplementTrie(PrefixTree).py
@@ -0,0 +1,39 @@
+class TrieNode:
+    def __init__(self):
+        self.word = False
+        self.children = collections.defaultdict(TrieNode)
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word :
+            node = node.children[char]
+        node.word = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word :
+            if char not in node.children : return False
+            else :
+                node = node.children[char]
+        
+        return node.word
+
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix :
+            if char not in node.children : return False
+            else : node = node.children[char]
+        
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
