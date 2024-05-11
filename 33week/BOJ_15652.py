e1ed79fea354c0ba0def89a3533b3bc4c11817ea
sjin4861 <tjwns6952@naver.com>
2024-05-11 21:19:17 +0900

    BOJ_15652 1. 비내림차순 구현을 위해 sequence 맨 마지막 값을 참조하는 조건 추가

---
+++ 33week/BOJ_15652.py
@@ -0,0 +1,26 @@
+# 자연수 N과 M이 주어졌을 때, 
+# 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+
+# 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+# 단 비내림차순이어야함!
+
+N, M = map(int, input().split())
+
+# back tracking
+
+sequence = []
+
+def fun(n):
+    if n == M:
+        for i in sequence:
+            print(i, end = " ")
+        print()
+        return
+    
+    for i in range(1, N+1):
+        if not sequence or sequence[-1] <= i:
+            sequence.append(i)
+            fun(n+1)
+            sequence.pop()
+
+fun(0)
