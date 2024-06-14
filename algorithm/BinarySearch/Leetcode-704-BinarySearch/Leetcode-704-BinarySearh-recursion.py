c8486269332ba04361f1aa7d626783c42ebd534d
miggule2 <miggule2@naver.com>
2024-06-14 11:20:13 +0900

    Leetcode-704-BinarySearch

---
+++ algorithm/BinarySearch/Leetcode-704-BinarySearch/Leetcode-704-BinarySearh-recursion.py
@@ -0,0 +1,17 @@
+class Solution:
+    index = 0
+    def search(self, nums: List[int], target: int) -> int:
+        if len(nums) == 0 : return -1
+        if len(nums) == 1 :
+            if nums[0] == target : return self.index
+            else : return -1
+        
+        mid = len(nums)//2
+        
+        if nums[mid] == target :
+            return self.index + mid
+        elif nums[mid] < target :
+            self.index += mid
+            return self.search(nums[mid:],target)
+        else :
+            return self.search(nums[:mid],target)
