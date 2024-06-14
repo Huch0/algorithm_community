b4e3d2e65d31f6b999b29aa754d9daeac6a7f930
miggule2 <miggule2@naver.com>
2024-06-14 11:50:39 +0900

    Move File

--- algorithm/sort/Leetcode-349-IntersectionofTwoArrays/Leetcode-349-IntersectionofTwoArrays-PythonBuiltInSet.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        nums1_set = set(nums1)
-        nums2_set = set(nums2)
-        return nums1_set & nums2_set
--- algorithm/sort/Leetcode-704-BinarySearch/Leetcode-704-BinarySearch-IndexFunction.py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        try : return nums.index(target)
-        except : return -1
