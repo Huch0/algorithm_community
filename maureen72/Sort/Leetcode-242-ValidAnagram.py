50a2f17df1a04964be3bfc9802ff18f51f3e44d8
maureen272 <maureen72@naver.com>
2024-07-06 19:56:48 +0900

    .\Leetcode-242-ValidAnagram

---
+++ maureen72/Sort/Leetcode-242-ValidAnagram.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if sorted(s) == sorted(t):
+            return True
+        else:
+            return False
+        
