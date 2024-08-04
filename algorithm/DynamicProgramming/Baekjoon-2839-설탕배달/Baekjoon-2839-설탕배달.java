64bd4b4efcc79a1c0c22b2edf8d3191054ca80e1
miggule2 <miggule2@naver.com>
2024-08-04 16:02:52 +0900

    Baekjoon-2839-설탕배달

---
+++ algorithm/DynamicProgramming/Baekjoon-2839-설탕배달/Baekjoon-2839-설탕배달.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Baekjoon_2839 {
+	public static int dynamic(int n,int count) {
+		if(n==1 || n==2) {return -1;}
+		
+		if(n==0) { return count;}
+		
+		n = n%5 == 0 ? n-5 : n-3;
+		
+		return dynamic(n,count+1);
+		
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		System.out.println(dynamic(n, 0));
+		
+	}
+
+}
