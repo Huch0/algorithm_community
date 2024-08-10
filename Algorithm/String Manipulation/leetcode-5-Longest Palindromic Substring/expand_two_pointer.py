4e9cf1a8516bbec58dc909b85f3397fce9db70c8
odinXD <97303233+odinXD@users.noreply.github.com>
2024-08-10 23:47:21 +0900

    Update expand_two_pointer.py

--- Algorithm/String Manipulation/leetcode-5-Longest Palindromic Substring/expand_two_pointer.py
+++ Algorithm/String Manipulation/leetcode-5-Longest Palindromic Substring/expand_two_pointer.py
@@ -4,11 +4,11 @@ class Solution:
             return s
         
         def expand(left : int, right : int) -> str:
-            while left >= 0 and right < len(s) and s[left:right+1] == s[left:right+1][::-1]:
+            while left >= 0 and right < len(s) and s[left] == s[right]:
                 left, right = left - 1, right + 1
             return s[left+1:right]
 
         result = ''
         for i in range(0, len(s) - 1):
-            result = max(result, expand(i, i+1), expand(i, i+2), key = len)
+            result = max(result, expand(i, i), expand(i, i+1), key = len)
         return result
