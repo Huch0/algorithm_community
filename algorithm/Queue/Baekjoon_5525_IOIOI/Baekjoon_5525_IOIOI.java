814b14f26cb672802c262995a3e54e5cd271aeaa
miggule2 <miggule2@naver.com>
2024-08-25 13:41:58 +0900

    Baekjoon_5525_IOIOI

---
+++ algorithm/Queue/Baekjoon_5525_IOIOI/Baekjoon_5525_IOIOI.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Baekjoon_5525_IOIOI {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Deque<Character> deque = new LinkedList<>();
+        int count = 0;
+        int deque_size = 0;
+
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        int length = sc.nextInt();
+        sc.nextLine();
+
+        String str = sc.nextLine();
+        char c = 'i';
+        for(int i = 0; i < length; i++){
+            if(deque_size == 0) {
+                if(str.charAt(i) == 'I') {deque.add('I'); deque_size++;}
+                continue;
+            }
+            
+            c = deque.peekLast();
+            if(c != str.charAt(i)){deque.add(str.charAt(i)); deque_size++;}
+            else {
+                deque.clear();
+                deque_size = 0;
+                if(c == 'I') {deque.add('I');deque_size++;}
+            }
+
+            if(deque_size == 2*n+1){
+                deque.pollFirst();
+                deque.pollFirst();
+                deque_size -= 2;    
+                count += 1;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
