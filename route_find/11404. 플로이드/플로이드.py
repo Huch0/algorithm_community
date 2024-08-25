3c3bba762c6fd3966b40b20bd35840c5e920d82a
hyeonjinha <mnbdj12345@gmail.com>
2024-08-25 16:54:50 +0900

    beakjoon-11404

---
+++ route_find/11404. 플로이드/플로이드.py
@@ -0,0 +1,39 @@
+import sys
+
+INF = int(1e9)  # 무한대 값 설정
+
+# 입력 받기
+n = int(sys.stdin.readline())
+m = int(sys.stdin.readline())
+
+# 2차원 리스트(그래프 표현)를 만들고, 모든 값을 무한으로 초기화
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+# 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if a == b:
+            graph[a][b] = 0
+
+# 각 간선에 대한 정보를 입력 받아, 그 값으로 초기화
+for _ in range(m):
+    a, b, c = map(int, sys.stdin.readline().split())
+    # 가장 짧은 간선 정보만 저장
+    graph[a][b] = min(c, graph[a][b])
+
+# 점화식에 따라 플로이드 워셜 알고리즘을 수행
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+# 수행된 결과를 출력
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        # 도달할 수 없는 경우, 0을 출력
+        if graph[a][b] == INF:
+            print(0, end=" ")
+        # 도달할 수 있는 경우 거리를 출력
+        else:
+            print(graph[a][b], end=" ")
+    print()
